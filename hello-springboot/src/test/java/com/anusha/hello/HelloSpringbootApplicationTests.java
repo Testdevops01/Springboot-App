7609a0ef6e402e437c4f5a950f9de5aae26ad9c9
Anusha <learningdevops0808@gmail.com>
2025-06-02 17:54:04 +0530

    Add files via upload

---
+++ hello-springboot/src/test/java/com/anusha/hello/HelloSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anusha.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
